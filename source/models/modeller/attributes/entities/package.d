cb755bedc64fa7b74083dbc39e7327d7596ce3d3
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-02 09:36:17 +0200

    Enhance attribute package structure

---
+++ source/models/modeller/attributes/entities/package.d
@@ -0,0 +1,1 @@
+module models.modeller.attributes.entities;
--- source/models/modeller/attributes/package.d
+++ source/models/modeller/attributes/package.d
@@ -1,1 +1,5 @@
 module models.modeller.attributes;
+
+public import models.modeller.attributes.elements;
+public import models.modeller.attributes.entities;
+public import models.modeller.attributes.uuids;

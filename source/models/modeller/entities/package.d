8cc0fe5ab626f5a597f9b6033ce69c267462576f
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-06-20 18:56:01 +0200

    Updates

--- source/models/modeller/entities/package.d
+++ source/models/modeller/entities/package.d
@@ -8,6 +8,7 @@ public import models.modeller.entities.app;
 public import models.modeller.entities.attribute;
 public import models.modeller.entities.class_;
 public import models.modeller.entities.component;
+public import models.modeller.entities.control;
 public import models.modeller.entities.entity;
 public import models.modeller.entities.function_;
 public import models.modeller.entities.interface_;
@@ -19,6 +20,6 @@ public import models.modeller.entities.package_;
 
 static this() {
   EntityRegistry
-  .register(MDLApi, MDLApp, MDLAttribute, MDLClass, MDLComponent, MDLEntity, MDLFunction, 
+  .register(MDLApi, MDLApp, MDLAttribute, MDLClass, MDLComponent, MDLControl, MDLEntity, MDLFunction, 
     MDLInterface, MDLLibrary, MDLMethod, MDLModel, MDLModule, MDLPackage);
 }
